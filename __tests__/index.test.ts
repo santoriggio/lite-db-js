efe7f31614678bcbd715cdcdc8cf9b3a0b556ee9
Santo <info@santoriggio.it>
2024-04-18 10:58:06 +0200

    first production

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -23,33 +23,59 @@ describe("test", () => {
 
     spaces.add([
       { id: generateUID(), title: "oo" },
-      { id: generateUID(), title: "aa", vote: 10 },
+      { id: fixedId, title: "oo" },
+      { id: generateUID(), title: "aa" },
     ]);
 
-    const query = spaces.where("title", "==", "oo");
+    const queryOO = spaces.where("title", "==", "oo");
+    const queryAA = spaces.where("title", "==", "aa");
 
-    query.on((snapshot) => {
-      console.log(snapshot.docs);
+    queryAA.on((snapshot) => {
+      console.log("AA", snapshot.docs);
     });
 
-    spaces.add([
-      { id: generateUID(), title: "aa", vote: 8 },
-      { id: generateUID(), title: "oo", vote: 3 },
-      { id: generateUID(), title: "oo", vote: 5 },
-    ]);
+    queryOO.on((snapshot) => {
+      console.log("OO", snapshot.docs);
+    });
 
-    // spaces.add({ id: generateUID(), title: "aa", vote: 7 });
+    spaces.doc(fixedId).update({
+      title: "aa",
+    });
+  });
 
-    // const doc = spaces.doc(fixedId);
+  it("test delete doc", () => {
+    const db = new DB();
+    const spaces = db.collection<Test>("spaces");
 
-    // doc.update({ title: "aaaa" });
+    spaces.add({
+      id: fixedId,
+      title: "AAA",
+    });
 
-    // console.log(spaces.docs)
+    const doc = spaces.doc(fixedId);
 
-    // console.log(spaces.docs)
+    doc.on((snapshot) => {
+      console.log(snapshot);
+    });
 
-    // spaces.docs.forEach((doc) => {
-    //   console.log(doc);
-    // });
+    doc.delete();
+  });
+});
+
+describe.only("edge case", () => {
+  it("large collection", () => {
+    const length = 100000;
+
+    const db = new DB();
+
+    const collection = db.collection<Test>("large");
+
+    collection.doc(fixedId).on((snapshot) => {
+      console.log(snapshot);
+    });
+
+    for (let i = 0; i < length; i++) {
+      collection.add({ id: i === 58472 ? fixedId : generateUID(), title: generateUID() });
+    }
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,33 @@ class DB {
     this._db.collections[path] = newCollection;
     return newCollection;
   }
+
+  doc<T>(id: string): Document<T> {
+    const splitted = id
+      .split("/")
+      .filter((str) => str.trim() !== "" && typeof str !== "undefined" && str !== null);
+
+    const docId = splitted.pop();
+
+    if (splitted.length === 0) {
+      throw Error("Collezione non valida");
+    }
+
+    const collectionId = splitted.join("/");
+
+    if (typeof this._db.collections[collectionId] === "undefined") {
+      throw Error("Collezione non trovata");
+    }
+
+    return this._db.collections[collectionId].doc<T>(docId);
+  }
 }
 
 class DBInstance {
   collections: Record<string, Collection<any>> = {};
   listeners: Listener<any>[] = [];
+  docListeners: DocListener<any>[] = [];
+
   constructor() {}
 
   notifyListeners() {
@@ -42,27 +64,127 @@ class DBInstance {
     });
   }
 
-  onAdd(doc: Document<any>) {
-    this.listeners.forEach((listener) => {
-      let isValid = true;
+  addDocListener(query: DocListener<any>["query"], callback: (snapshot: any) => void) {
+    const uniqueId = generateUID();
+    this.docListeners.push({
+      id: uniqueId,
+      func: callback,
+      query,
+      _remove: () => {
+        this.docListeners.filter((listener) => listener.id !== uniqueId);
+      },
+    });
+  }
 
+  onAdd(a: Document<any> | Document<any>[]) {
+    let toCall: Record<string, Listener<any>> = {};
+    const list = Array.isArray(a) ? a : [a];
+
+    for (let i = 0; i < this.listeners.length; i++) {
+      const listener = this.listeners[i];
+
+      for (let j = 0; j < list.length; j++) {
+        if (toCall[listener.id]) {
+          continue;
+        }
+
+        const doc = list[j];
+        const filters = listener.query.filters;
+        let isValid = true;
+
+        filters.forEach((filter) => {
+          if (typeof doc.data[filter.key] === "undefined") isValid = false;
+
+          if (filter.operator === "==") {
+            if (doc.data[filter.key] != filter.value) isValid = false;
+          }
+        });
+
+        if (isValid) {
+          toCall = {
+            ...toCall,
+            [listener.id]: listener,
+          };
+        }
+      }
+    }
+
+    Object.values(toCall).forEach((listener) => {
+      listener.func(listener.query);
+    });
+  }
+
+  onEdit(prev: DocumentData, current: Document<any>) {
+    this.listeners.forEach((listener) => {
       const filters = listener.query.filters;
 
+      let isValid = [true, true];
+
       filters.forEach((filter) => {
-        if (typeof doc.data[filter.key] === "undefined") isValid = false;
+        if (typeof prev[filter.key] === "undefined") isValid[0] = false;
+        if (typeof current.data[filter.key] === "undefined") isValid[1] = false;
 
         if (filter.operator === "==") {
-          if (doc.data[filter.key] != filter.value) isValid = false;
+          if (prev[filter.key] != filter.value) isValid[0] = false;
+          if (current.data[filter.key] != filter.value) isValid[1] = false;
         }
       });
 
-      if (isValid) {
+      if (isValid.includes(true)) {
+        listener.func(listener.query);
+      }
+    });
+
+    this.docListeners.forEach((listener) => {
+      if (listener.query.id === current.id) {
         listener.func(listener.query);
       }
     });
   }
 
-  onEdit(prev: Document<any>, current: Document<any>) {}
+  onDelete(a: Document<any>) {
+    let toCall: Record<string, Listener<any>> = {};
+    const list = Array.isArray(a) ? a : [a];
+
+    for (let i = 0; i < this.listeners.length; i++) {
+      const listener = this.listeners[i];
+
+      for (let j = 0; j < list.length; j++) {
+        if (toCall[listener.id]) {
+          continue;
+        }
+
+        const doc = list[j];
+        const filters = listener.query.filters;
+        let isValid = true;
+
+        filters.forEach((filter) => {
+          if (typeof doc.data[filter.key] === "undefined") isValid = false;
+
+          if (filter.operator === "==") {
+            if (doc.data[filter.key] != filter.value) isValid = false;
+          }
+        });
+
+        if (isValid) {
+          toCall = {
+            ...toCall,
+            [listener.id]: listener,
+          };
+        }
+      }
+    }
+
+    Object.values(toCall).forEach((listener) => {
+      listener.func(listener.query);
+    });
+
+    this.docListeners.forEach((listener) => {
+      if (listener.query.id === a.id) {
+        listener.func(listener.query);
+      }
+    });
+  }
 }
 
 class Query<T> {
@@ -87,6 +209,10 @@ class Query<T> {
 
   get filters() {
     return this._filters;
+  }
+
+  get count() {
+    return this.docs.length;
   }
 
   get docs() {
@@ -120,17 +246,22 @@ class Query<T> {
   }
 
   where(key: string, operator: "==", value: any) {
-    this._filters.push({ key, operator, value });
-
-    return new Query<T>(this._db, this.path, this._filters, this._listeners);
+    return new Query<T>(
+      this._db,
+      this.path,
+      [...this._filters, { key, operator, value }],
+      this._listeners
+    );
   }
 
   on(callback: (snapshot: Query<T>) => void) {
+    callback(this);
+
     this._db.addListener(this, callback);
   }
 }
 
-class Collection<T extends DocumentData> extends Query<T> {
+class Collection<T extends DocumentData = DocumentData> extends Query<T> {
   private _id: string;
   private _hashmap: Record<string, Document<T>> = {};
   private _docs: Document<T>[] = [];
@@ -141,7 +272,29 @@ class Collection<T extends DocumentData> extends Query<T> {
     this._id = id;
   }
 
-  doc(id: string) {}
+  doc<E extends T>(id: string): Document<E | T> {
+    if (this._hashmap[id]) {
+      return this._hashmap[id];
+    }
+
+    const document = new Document<T>(this.db, id, this._id);
+
+    this.db.collections[this._id].docs.push(document);
+    this._hashmap[id] = document;
+    this.db.onAdd(document);
+
+    return document;
+  }
+
+  deleteDoc(doc: Document<T>) {
+    if (typeof this._hashmap[doc.id] !== "undefined") {
+      delete this._hashmap[doc.id];
+
+      this._docs = this._docs.filter((item) => item.id !== doc.id);
+
+      this.db.onDelete(doc);
+    }
+  }
 
   get id() {
     return this._id;
@@ -163,21 +316,35 @@ class Collection<T extends DocumentData> extends Query<T> {
     // Aggiungi logica per aggiungere documenti come desiderato
     // Questo è solo un esempio
     if (Array.isArray(data)) {
+      let added: Document<T>[] = [];
+
       data.forEach((item) => {
         const uniqueId = item.id || generateUID();
-        const document = new Document<T>(uniqueId, this._id, item);
+
+        if (typeof this._hashmap[uniqueId] !== "undefined") {
+          this._hashmap[uniqueId].update(item);
+        } else {
+          const document = new Document<T>(this.db, uniqueId, this._id, item);
+
+          coll.docs.push(document);
+          this._hashmap[uniqueId] = document;
+          added.push(document);
+        }
+      });
+
+      this.db.onAdd(added);
+    } else {
+      const uniqueId = data.id || generateUID();
+
+      if (typeof this._hashmap[uniqueId] !== "undefined") {
+        this._hashmap[uniqueId].update(data);
+      } else {
+        const document = new Document<T>(this.db, uniqueId, this._id, data);
 
         coll.docs.push(document);
         this._hashmap[uniqueId] = document;
         this.db.onAdd(document);
-      });
-    } else {
-      const uniqueId = data.id || generateUID();
-      const document = new Document<T>(uniqueId, this._id, data);
-
-      coll.docs.push(document);
-      this._hashmap[uniqueId] = document;
-      this.db.onAdd(document);
+      }
     }
   }
 
@@ -209,23 +376,57 @@ class Document<T extends DocumentData> {
   private _id: string;
   private _path: string;
   private _data: T;
+  private _db: DBInstance;
+  private _exists: boolean = true;
 
-  constructor(id: string, collection: string, data?: T) {
+  constructor(db: DBInstance, id: string, collection: string, data?: T) {
     this._id = id;
     this._data = data;
+    this._path = collection;
+    this._db = db;
   }
 
   set(data: T) {
+    if (this._exists === false) {
+      throw Error("This document don't exists");
+    }
+
+    const prev = this._data;
+
     this._data = data;
+    this._db.onEdit(prev, this);
   }
 
   update(data: Partial<T>) {
+    if (this._exists === false) {
+      throw Error("This document don't exists");
+    }
+
+    const prev = this._data;
+
     this._data = {
       ...this._data,
       ...data,
     };
+
+    this._db.onEdit(prev, this);
   }
 
+  delete() {
+    this._exists = false;
+
+    this._db.collections[this._path].deleteDoc(this);
+  }
+
+  on(callback: (snapshot: Document<T>) => void) {
+    callback(this);
+
+    this._db.addDocListener(this, callback);
+  }
+
+  get exists() {
+    return this._exists;
+  }
   get data() {
     return this._data;
   }
@@ -246,6 +447,13 @@ type DocumentData = {
 type Listener<T> = {
   id: string;
   query: Query<T>;
+  func: (snapshot: T) => void;
+  _remove: () => void;
+};
+
+type DocListener<T> = {
+  id: string;
+  query: Document<T>;
   func: (snapshot: T) => void;
   _remove: () => void;
 };
